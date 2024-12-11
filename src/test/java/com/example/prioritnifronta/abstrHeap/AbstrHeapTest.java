4d501d6ff4360fbd00670fceadb6d7961cf5b04d
Martin Václavík <146117943+MartinVaclavik1@users.noreply.github.com>
2024-12-11 19:36:32 +0100

    test na reorganizaci

--- src/test/java/com/example/prioritnifronta/abstrHeap/AbstrHeapTest.java
+++ src/test/java/com/example/prioritnifronta/abstrHeap/AbstrHeapTest.java
@@ -3,7 +3,9 @@ package com.example.prioritnifronta.abstrHeap;
 import com.example.prioritnifronta.Obec;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,6 +56,7 @@ class AbstrHeapTest {
     @Test
     void vloz004() throws AbstrHeapException {
         IAbstrHeap<Obec> prioritniFronta = new AbstrHeap<>();
+        Obec.zmenPrioritu();
         prioritniFronta.reorganizace();
         prioritniFronta.vloz(T2);
         prioritniFronta.vloz(T1);
@@ -64,6 +67,7 @@ class AbstrHeapTest {
     @Test
     void vloz005() throws AbstrHeapException {
         IAbstrHeap<Obec> prioritniFronta = new AbstrHeap<>();
+        Obec.zmenPrioritu();
         prioritniFronta.reorganizace();
         prioritniFronta.vloz(T2);
         prioritniFronta.vloz(T1);
@@ -71,6 +75,34 @@ class AbstrHeapTest {
         prioritniFronta.vloz(T5);
     }
 
+    @Test
+    void vloz006() throws AbstrHeapException {
+        IAbstrHeap<Obec> prioritniFronta = new AbstrHeap<>();
+        Obec.zmenPrioritu();
+        prioritniFronta.reorganizace();
+
+        List<Obec> list = new ArrayList<>();
+        list.add(0,null);
+        list.add(T2);
+        list.add(T1);
+        list.add(T3);
+        list.add(T5);
+        prioritniFronta.vybuduj(list);
+        //T1 má jednoho potomka T5, který je vyšší, tak se prohodí
+        //T2 bude mít potomka T5 a T3. T5 je nejvyšší ze dvojice a vyšší, než T2, tak se prohodí a pořadí je 5,2,3,1
+
+        List<Obec> list2 = new ArrayList<>();
+        list2.add(T5);
+        list2.add(T2);
+        list2.add(T3);
+        list2.add(T1);
+
+        Iterator<Obec> it = prioritniFronta.vytvorIterator(eTypProhl.DO_SIRKY);
+
+        for (Obec obec: list2){
+            assertEquals(obec,it.next());
+        }
+    }
     @Test
     void odeber001() throws AbstrHeapException {
         IAbstrHeap<Obec> prioritniFronta = new AbstrHeap<>();
